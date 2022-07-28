5f1bc6de8fc2c099936cd5a6b818354913cc8232
Jihyun Lee <marchofjh@gmail.com>
2022-07-28 23:51:29 +0900

    MyCircularQueue newly uploaded

---
+++ Baekjoon/src/queue/MyCircularQueue.java
@@ -0,0 +1,44 @@
+package queue;
+
+public class MyCircularQueue<T> implements IQueue<T> {
+
+	private T[] elements;
+	private int front
+	
+	@Override
+	public void offer(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
