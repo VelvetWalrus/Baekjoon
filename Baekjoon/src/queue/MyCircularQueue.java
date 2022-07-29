a7e75f4d76beb092639e9720fb51e5b12f409513
Jihyun Lee <marchofjh@gmail.com>
2022-07-29 23:23:48 +0900

    MyCircularQueue.java

--- Baekjoon/src/queue/MyCircularQueue.java
+++ Baekjoon/src/queue/MyCircularQueue.java
@@ -3,42 +3,67 @@ package queue;
 public class MyCircularQueue<T> implements IQueue<T> {
 
 	private T[] elements;
-	private int front
+	private int front;
+	private int rear;
+	private int maxSize;
+	
+	public MyCircularQueue(int size) {
+		this.elements = (T[]) new Object[size + 1];
+		this.front = 0;
+		this.rear = 0;
+		this.maxSize = size + 1;
+	}
 	
 	@Override
 	public void offer(T data) {
-		// TODO Auto-generated method stub
+		if (this.isFull()) {
+			throw new IllegalStateException();
+		}
+		
+		this.rear = (this.rear + 1) % this.maxSize;
+		this.elements[this.rear] = data;
 		
 	}
 
 	@Override
 	public T poll() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isEmpty()) {
+			throw new IllegalStateException();
+		} 
+		this.front = (this.front + 1) % this.maxSize;
+		return this.elements[this.front];
 	}
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isEmpty()) {
+			throw new IllegalStateException();
+		}
+		
+		return this.elements[this.front + 1];
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (this.front <= this.rear) {
+			return this.rear = this.front;
+		}
+		return this.maxSize - this.front + this.rear;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.front = 0;
+		this.rear = 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.front == this.rear;
+	}
+	
+	private boolean isFull() {
+		return (this.rear + 1) % this.maxSize == this.front;
 	}
 
 }
