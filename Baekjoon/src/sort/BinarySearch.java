cfd066c16b1bfa191f7aa6ff75ca92b2994de1d7
Jihyun Lee <marchofjh@gmail.com>
2022-08-02 22:53:20 +0900

    Binary Search newly uploaded

---
+++ Baekjoon/src/sort/BinarySearch.java
@@ -0,0 +1,15 @@
+package sort;
+
+public class BinarySearch {
+
+	public int search(int[] arr, int target) {
+		// 1. 데이터의 중간 인덱스 값을 찾는다.
+		// 2. 중간 인덱스 위치를 기준으로 arr을 절반으로 나눈다. 
+		// 3. 나눠진 절반의 리스트에서 tartget 값을 찾는다. 
+		
+		
+		
+		return -1;
+	}
+	
+}
